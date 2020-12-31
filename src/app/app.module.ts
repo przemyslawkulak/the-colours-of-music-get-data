cda00b1d6776e5388dcdf2900205f3a6ae96bd2d
Przemysław Kulak <przemyslaw.kulak86@gmail.com>
2020-12-31 15:38:25 +0100

    feat: added Material imports to module, feat: added proxy conf,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 // core Module
 import { CoreModule } from '@core/core.module';
@@ -10,13 +11,37 @@ import { CoreModule } from '@core/core.module';
 // shared Module
 import { SharedModule } from '@shared/shared.module';
 
+// angular material
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatRippleModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+
 // rest imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, CoreModule, SharedModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    CoreModule,
+    SharedModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatRippleModule,
+    MatButtonModule,
+    MatDividerModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
