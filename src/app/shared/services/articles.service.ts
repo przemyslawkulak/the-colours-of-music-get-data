502c2339f78b0ae2647a86417560b69ae471a900
Przemysław Kulak <przemyslaw.kulak86@gmail.com>
2020-12-31 15:39:36 +0100

    feat: created article Service

---
+++ src/app/shared/services/articles.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ArticlesService {
+  constructor(private _apiService: ApiService) {}
+
+getArticles(): Observable<any>{
+  return this._apiService.get('articles/')
+}
+}
