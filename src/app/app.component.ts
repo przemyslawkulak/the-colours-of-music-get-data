29a61e16b0df1514c8909382ca2f069772b61b35
Przemysław Kulak <przemyslaw.kulak86@gmail.com>
2020-12-31 15:42:03 +0100

    feat: created circular menu, feat: added world map svg, feat: showing article list on front page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from '@shared/interfaces/article';
+import { ArticlesService } from '@shared/services/articles.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'the-colours-of-music';
+  showFiller = false;
+  opened = false;
+  articles: Article[];
+
+  constructor(private _articlesService: ArticlesService) {}
+
+  ngOnInit() {
+    this.getAllArticles();
+  }
+  getAllArticles() {
+    this._articlesService.getArticles().subscribe((response) => {
+      this.articles = response;
+    });
+  }
 }
